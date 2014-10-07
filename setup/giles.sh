160be00ea443000989056f161e0abd2120faf85a
Gabe Fierro <gtfierro225@gmail.com>
2014-10-07 13:04:58 -0700

    install giles to home dir, take care of other dependencies

--- setup/giles.sh
+++ setup/giles.sh
@@ -1,7 +1,11 @@
 # bash script to setup environment on Ubuntu 14.04 for giles
 
-sudo apt-get install golang git mongodb
+sudo add-apt-repository ppa:cal-sdb/smap
+sudo apt-get update
+sudo apt-get install -y golang git mongodb mercurial libprotobuf-dev readingdb python-pip
 
+cd
+GOPATH=`pwd`/go
 git clone https://github.com/gtfierro/giles
 cd giles/giles
 go get code.google.com/p/goprotobuf/proto
@@ -10,3 +14,5 @@ go get github.com/gorilla/mux
 go get gopkg.in/mgo.v2
 go get gopkg.in/mgo.v2/bson
 go build
+
+# supervisor file
