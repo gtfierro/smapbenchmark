ac1800e6e287f93113aa70c30cda5bf71ce7983c
Gabe Fierro <gtfierro225@gmail.com>
2014-10-07 13:11:12 -0700

    auto install for archiver

--- setup/archiver.sh
+++ setup/archiver.sh
@@ -3,9 +3,10 @@
 # install infrastructure, then update from git
 sudo add-apt-repository ppa:cal-sdb/smap
 sudo apt-get update
-sudo apt-get install python-smap readingdb powerdb2 git python-pip
+sudo apt-get install -y python-smap readingdb powerdb2 git python-pip
 
 # get latest version
+cd
 git clone http://github.com/SoftwareDefinedBuilding/smap
 cd smap/python
 sudo pip install -e .
