78b72d874f38e5ffa74a0caae4fde366bc1e64db
Gabe Fierro <gtfierro225@gmail.com>
2014-10-07 21:01:39 -0700

    add some analysis stuff

---
+++ analysis/parselog.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import re
+import sys
+
+def parsegileslog(filename):
+    line_regex = re.compile(r'(\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}) UUID Cache:(\d+)--Repub clients:(\d+)--Recv Adds:(\d+)--Pend Write:(\d+)--Live Conn:(\d+)')
+    lines = []
+    with open(filename) as f:
+        for l in f.readlines():
+            found = line_regex.findall(l)
+            if found:
+                lines.append(found[0])
+    d = pd.DataFrame(lines)
+    d.columns = ['ts','cache','repub','recv','write','live']
+    d['ts'] = pd.to_datetime(d['ts'])
+    d['cache'] = d['cache'].astype(int)
+    d['repub'] = d['repub'].astype(int)
+    d['recv'] = d['recv'].astype(int)
+    d['write'] = d['write'].astype(int)
+    d['live'] = d['live'].astype(int)
+    return d
+
+if __name__=='__main__':
+    import matplotlib.pyplot as plt
+    d = parsegileslog(sys.argv[1])
+    axis = d[['recv','write']].plot(figsize=(20,15))
+    fig = axis.get_figure()
+    fig.savefig('out.png')
