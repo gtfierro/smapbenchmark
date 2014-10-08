c4e3088dcf5f1a6e3b77bc50577b2f2f33253cfc
Gabe Fierro <gtfierro225@gmail.com>
2014-10-07 17:54:13 -0700

    remove unrelated comment

--- start.py
+++ start.py
@@ -44,12 +44,6 @@ elif args.start_sources:
         command = "twistd --pidfile {0}.pid -n smap {0}.ini".format(ini)
         p = subprocess.Popen(command, shell=True, stdout=FNULL)
         processes.append(p)
-    # run smap query to see if the streams are registered
-    # This is partly to get around the race condition arising when
-    # a driver subscribes to the output of another
-    # However, top handle the mutual dependence case, it really needs to
-    # restart the services
-    # Note that this encodes the uri of the archiver
     try:
         raw_input()
     except:
