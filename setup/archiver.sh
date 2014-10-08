94d1c0fed03939c55e0c375d2b9b0b098a3a7b90
Gabe Fierro <gtfierro225@gmail.com>
2014-10-08 12:14:09 -0700

    stick with apt packages for smap

--- setup/archiver.sh
+++ setup/archiver.sh
@@ -4,9 +4,3 @@
 sudo add-apt-repository ppa:cal-sdb/smap
 sudo apt-get update
 sudo apt-get install -y python-smap readingdb powerdb2 git python-pip
-
-# get latest version
-cd
-git clone http://github.com/SoftwareDefinedBuildings/smap
-cd smap/python
-sudo pip install -e .
