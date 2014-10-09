e9ac06c4bc0634adc16aa8f26c302d188bab7159
Gabe Fierro <gtfierro225@gmail.com>
2014-10-09 00:50:56 -0700

    reliably get IPs; wait for full startup

--- aws/configure_aws.py
+++ aws/configure_aws.py
@@ -1,18 +1,20 @@
 import boto.ec2
 from configobj import ConfigObj
+import time
 
 config = ConfigObj('aws.ini')['aws']
 print config
-AWS_SECRET_ACCESS_KEY=config['aws']['AWS_SECRET_ACCESS_KEY']
-AWS_ACCESS_KEY=config['aws']['AWS_ACCESS_KEY']
-smap_ami = config['aws']['image_id']
-key_name = config['aws']['key_name']
-instance_type = config['aws']['instance_type']
-security_group_ids = list(config['aws']['security_group_ids'])
-subnet_id = config['aws']['subnet_id']
-region = config['aws']['region']
+AWS_SECRET_ACCESS_KEY=config['AWS_SECRET_ACCESS_KEY']
+AWS_ACCESS_KEY=config['AWS_ACCESS_KEY']
+smap_ami = config['image_id']
+key_name = config['key_name']
+instance_type = config['instance_type']
+security_group_ids = config['security_group_ids']
+if not isinstance(security_group_ids, list):
+    security_group_ids = [security_group_ids]
+subnet_id = config['subnet_id']
+region = config['region']
 
-#conn = boto.ec2.connect_vpc('vpc-3547ab50',
 conn = boto.ec2.connect_to_region(region,
     aws_access_key_id=AWS_ACCESS_KEY,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
@@ -24,23 +26,19 @@ def create_security_group(conn):
 
 def get_instance(conn, ami, key, instance, security_groups, subnet):
     reservation = conn.run_instances(ami, key_name=key, instance_type=instance, security_group_ids=security_groups, subnet_id=subnet_id)
-    return reservation.instances
-
-def assign_ip(conn, instance):
-    a = conn.allocate_address('vpc')
-    try:
-        a.associate(instance.id)
-    except Exception as e:
-        a.release()
-    return a
+    instance = reservation.instances[0]
+    instance.update()
+    while instance.state == 'pending':
+        print instance,instance.state,instance.ip_address
+        time.sleep(5)
+        instance.update()
+    return instance
 
 def create_some(num):
     ips = []
     for i in range(num):
-        instance = get_instance(conn, smap_ami, key_name, instance_type, security_group_ids, subnet_id)[0]
-        a = assign_ip(conn, instance)
-        ips.append(a.public_ip)
-        print a.public_ip
+        instance = get_instance(conn, smap_ami, key_name, instance_type, security_group_ids, subnet_id)
+        ips.append(instance.ip_address)
     return ips
 
 if __name__ == '__main__':
